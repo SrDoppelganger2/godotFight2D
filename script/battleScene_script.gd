de9243924381deb96d4d4f7ced9fa177fe5473e1
srdoppelganger <arthur.albuquerque2004@gmail.com>
2025-07-04 11:03:24 -0300

    added placholder background and silly sound

--- script/battleScene_script.gd
+++ script/battleScene_script.gd
@@ -3,11 +3,7 @@ extends Node2D
 @onready var meme: AudioStreamPlayer2D = $Sounds/meme_intro
 
 
-# Called when the node enters the scene tree for the first time.
+
+# nem sei se esse script vai ser util pra alguma coisa, fiz puramente pelo meme
 func _ready() -> void:
 	meme.play();
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
